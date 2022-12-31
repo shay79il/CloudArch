f545c7d105c87e3d2309c2db225ded83295aa45f
Shay Goldstein <shay79il@gmail.com>
2022-12-31 18:02:00 +0200

    update kind configuration

--- app/order.py
+++ app/order.py
@@ -1,5 +1,5 @@
 import requests
-api_url = "http://127.0.0.1:5000/"
+api_url = "http://127.0.0.1:30007/"
 order = {"pizza": "napolitana", "size": "family", "amount": 2}
 response = requests.post(api_url+"order", json=order)
 print(response.json())
